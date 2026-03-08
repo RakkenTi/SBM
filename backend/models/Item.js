0c30723431b7aade98f7d71f1da27dac59d61717
Abdi <thatinferno.poo@gmail.com>
2026-03-08 17:22:57 -0400

    added schemas for product and sprint items

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -1,8 +1,28 @@
 const mongoose = require("mongoose");
 
 const ItemSchema = new mongoose.Schema({
-  name: String,
-  value: Number
+  itemName: { type: String, required: true },
+
+  itemType: {
+    type: String,
+    enum: ["Sprint", "Product"]
+  },
+
+  itemPriority: {
+    type: String,
+    enum: ["High", "Medium", "Low"]
+  },
+
+  itemRisk: {
+    type: String,
+    enum: ["High", "Medium", "Low"]
+  },
+
+  team: String,
+
+  itemDesc: String,
+
+  inProgress: Boolean
 });
 
 module.exports = mongoose.model("Item", ItemSchema);
