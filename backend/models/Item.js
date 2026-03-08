3b0dd96d9a11d01738275e2b0728f54daa161233
RakkenTi <martin3zmrk@gmail.com>
2026-03-08 19:50:26 -0400

    Updated backend to seperate the functionality of server.js into modules. Routes are now defined in the routes/ directory, and a seperate mongoose lifecycle observer is created in mongoose/. Also fixed syntax errors in models/Item.js

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -4,13 +4,12 @@ const ItemSchema = new mongoose.Schema({
   title: { type: String, required: true},
   description: String,
   type: {type: String, enum: ['Product', 'Sprint'], default: 'Product' },
-  priority: {type: String, enum: ['High', 'Medium', 'Low'], defualt: 'Medium'},
+  priority: {type: String, enum: ['High', 'Medium', 'Low'], default: 'Medium'},
   status: {type: String, enum: ['To Do', 'In Progress', 'Done'], default: 'To Do'},
   effort: {type: Number, default: 0},
   risk: {type: String, enum: ['Low', 'Medium', 'High'], default: 'Low'},
   teamLabel: String,
   isLocked: {type: Boolean, default: false},
-  // possibly adding more in the future for 'Engineering Tasks' if time permits
 });
 
 module.exports = mongoose.model("Item", ItemSchema);
