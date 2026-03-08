2105d623bb4ca3d0d23c92736d22df35a3861a9a
Abdi <thatinferno.poo@gmail.com>
2026-03-07 19:17:06 -0500

    tailwind stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,8 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = createSignal(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        test changing this thing
-      </p>
-    </>
+    <div class="min-h-screen bg-zinc-900 text-white flex items-center justify-center">
+      <h1 class="text-4xl font-bold">Tailwind is working</h1>
+    </div>
   )
 }
 
