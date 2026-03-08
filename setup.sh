0dbbc0ba4917b831d32f29acdc058cbd54b101eb
RakkenTi <martin3zmrk@gmail.com>
2026-03-08 03:40:39 -0400

    Modified setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$PROJECT_DIR"
+echo "Setting up project.."
 echo "Setting up frontend folder.."
 cd frontend && npm install
 echo "Setting up backend.."
-cd backend && npm install && touch .env
-echo "Setup complete!"
+cd ../backend && npm install && touch .env
+echo "Setup complete! Make sure to fill out the .env!"
