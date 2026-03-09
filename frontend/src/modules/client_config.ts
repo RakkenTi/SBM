b4aba82efaea115235b0b365bb79d9afb12b2ead
RakkenTi <martin3zmrk@gmail.com>
2026-03-09 08:51:59 -0400

    Created master scripts, setup npm workspaces, added concurrently for easier local testing

--- frontend/src/modules/client_config.ts
+++ frontend/src/modules/client_config.ts
@@ -1,2 +1,2 @@
-const localURL = 'http://localhost:5000'
+const localURL = 'http://localhost:8080'
 export const url = import.meta.env.VITE_EXPRESS_URL || localURL
--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 set -e
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 npm run install:all
+cd backend && touch .env
 echo "Setup complete! Make sure to fill out the .env!"
