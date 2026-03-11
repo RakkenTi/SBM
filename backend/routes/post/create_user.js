02b0af85cedaa1c85d8155cf79bc9f4163ee3f7f
Rory Howe-Borges <rhoweborges@gmail.com>
2026-03-11 19:38:48 -0400

    added bcrypt and impemented for encryped password

--- backend/routes/post/create_user.js
+++ backend/routes/post/create_user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcrypt') //add bcyrpt for password encryption
 const router = express.Router() // create a router for this file
 const User = require('../../models/User')
 
@@ -7,6 +8,14 @@ router.post('/create_user', async (req, res) => {
         // get user data sent from the frontend
         const { firstName, lastName, userID, password } = req.body
 
+        const existingUser = await User.findOne({ userID }) //check if userID exists 
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'user already exists'}) 
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 8)
+
         console.log('Received request to create user:', firstName, lastName, userID) // for debugging
         
         // create a new user object using the schema
@@ -14,7 +23,7 @@ router.post('/create_user', async (req, res) => {
             firstName,
             lastName,
             userID,
-            password
+            password: hashedPassword
         })
 
         // save the user to MongoDB
--- backend/routes/post/login.js
+++ backend/routes/post/login.js
@@ -1,5 +1,5 @@
 // this is just checking if userID exists - for log in
-
+const bcrypt = require('bcrypt') // add bcrypt for password encryption
 const express = require('express')
 const router = express.Router() // create router for this file
 const User = require('../../models/User')
@@ -7,17 +7,19 @@ const User = require('../../models/User')
 router.post('/login', async (req, res) => {
     try {
         const { userID, password } = req.body // get userID from front end
+
         const user = await User.findOne({ userID }) // search db for userID
+        
         if (!user) {
             return res.status(404).json({ message: 'user not found' }) // not found user
         } 
+        
+        const match = await bcrypt.compare(password, user.password) // compare password
 
-        if (user.password !== password) {
+        if (!match) {
             return res.status(401).json({ message: 'Incorrect password' }) // password doesn't match
         }
 
-        
-        
         res.status(200).json({ message: 'login successful' }) // success
 
     } catch (error){
