6932f042685ea25bfb42a4e730e4951c362929a7
RakkenTi <martin3zmrk@gmail.com>
2026-03-24 22:49:33 -0400

    Latest

--- frontend/src/components/inputbox.tsx
+++ frontend/src/components/inputbox.tsx
@@ -2,30 +2,6 @@ import { Component, JSXElement, splitProps } from 'solid-js'
 import InputFrame from './inputframe'
 import { JSX } from 'solid-js/h/jsx-runtime'
 
-type ValidInputType =
-    | 'button'
-    | 'checkbox'
-    | 'color'
-    | 'date'
-    | 'datetime-local'
-    | 'email'
-    | 'file'
-    | 'hidden'
-    | 'image'
-    | 'month'
-    | 'number'
-    | 'password'
-    | 'radio'
-    | 'range'
-    | 'reset'
-    | 'search'
-    | 'submit'
-    | 'tel'
-    | 'text'
-    | 'time'
-    | 'url'
-    | 'week'
-
 interface inputboxProps extends JSX.InputHTMLAttributes<HTMLInputElement> {
     label: string
 }
